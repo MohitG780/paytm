d55c256fffa5941f30d3c75a7582e61e91cf70b3
MohitG780 <mohitgupta000780@gmail.com>
2024-06-19 17:08:50 +0530

    updated

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 mongoose.connect("mongodb://localhost:27017/paytm");
 
 const userSchema=mongoose.Schema({
-    username:String.email(),
+    username:String,
     password:String,
     firstname:String,
     lastname:String
