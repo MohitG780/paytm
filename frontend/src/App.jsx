94aa1e5886e4a9e6cf8d9ca9ea8b17daadccce7f
MohitG780 <mohitgupta000780@gmail.com>
2024-06-19 21:11:44 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-
+import { Route,BrowserRouter,Routes } from "react-router-dom"
+import  React from "react"
+import { Dashboard } from "./pages/Dashboard";
+import { Signup } from "./pages/Signup"
+import { Signin } from "./pages/Signin";
+import { SendMoney } from "./pages/SendMoney";
 function App() {
+
   return (
-    <>
-       <BrowserRouter>
-        <Routes>
+    <BrowserRouter>
+      <Routes>
           <Route path="/signup" element={<Signup />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
           <Route path="/signin" element={<Signin />} />
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/send" element={<SendMoney />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+      </Routes>
+    </BrowserRouter>
   )
 }
-export default App;
+
+export default App
